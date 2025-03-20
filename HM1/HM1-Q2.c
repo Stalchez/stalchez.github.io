ea81cd69c51f44bd9cde6ed6cd43c1be26b671af
Emirhan <110253408+Stalchez@users.noreply.github.com>
2025-03-20 19:59:04 +0300

    Update HM1-Q2.c

--- HM1/HM1-Q2.c
+++ HM1/HM1-Q2.c
@@ -6,11 +6,11 @@
 /* function main begins program execution */
 int main( void )
 {
-   int integer1; /* first number to be input by user  */    // add ; to the end
-   int integer2; /* second number to be input by user */    // add ; to the end
-   int sum;      /* variable in which sum will be stored */     // the int sum doesn't exist we need to interchange value x and sum
+   int integer1; /* first number to be input by user  */    // (ERROR!)-> add ; to the end
+   int integer2; /* second number to be input by user */    // (ERROR!)-> add ; to the end
+   int sum;      /* variable in which sum will be stored */     // (ERROR!)-> the int sum doesn't exist we need to interchange value x and sum
 
-   printf( "Enter first integer\n" ); /* prompt */    // the stringer doesnt written in "" 
+   printf( "Enter first integer\n" ); /* prompt */    // (ERROR!)-> the stringer didn't written in "" 
    scanf( "%d", &integer1 );        /* read an integer */
 
    printf( "Enter second integer\n" ); /* prompt */
@@ -22,6 +22,8 @@ int main( void )
    return 0; /* indicate that program ended successfully */
 
 } /* end function main */
+
+//(Q1)-> now we can start the code with integer1=4 and integer2=5, 
 
 // The code is working now.
 
